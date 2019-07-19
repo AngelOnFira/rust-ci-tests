fa69365ae82d402d047c9ec3514cb177b0111f53
Forest <wizcardforest@gmail.com>
2019-07-18 23:50:07 -0400

    Added more from the rand book

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,15 @@ use rand::prelude::*;
 fn main() {
     let x: u8 = random();
     println!("{}", x);
+
+    // If we want to be a bit more explicit (and a little more efficient) we can
+    // make a handle to the thread-local generator:
+    let mut rng = thread_rng();
+    if rng.gen() { // random bool
+        let x: f64 = rng.gen(); // random number in range [0, 1)
+        let y = rng.gen_range(-10.0, 10.0);
+        println!("x is: {}", x);
+        println!("y is: {}", y);
+        println!("Number from 0 to 9: {}", rng.gen_range(0, 10));
+    }
 }
